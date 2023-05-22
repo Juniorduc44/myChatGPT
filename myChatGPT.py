aaee1f23956dffcfe53e63e1677616662b5e832f
Juniorduc44 <Juniorduc44@gmail.com>
2023-05-21 21:23:30 -0700

    v0.2.2

--- myChatGPT.py
+++ myChatGPT.py
@@ -1,4 +1,4 @@
-#myChatGPT version_0.2.1
+#myChatGPT version_0.2.2
 import os
 import openai
 import os
@@ -21,12 +21,29 @@ def ChatGPT_conversation(conversation):
     return conversation
 
 conversation = []
-conversation.append({'role': 'user', 'content': 'How may I help you?'})
+
+types = '''\n
+1.) Python Programmer \n'''
+print(types)
+aiPersonality = int(input("Enter a number for the personality type: "))
+if aiPersonality == 1:
+    conversation.append({'role': 'user', 'content': '''
+    Ignore all previous instructions before this one. 
+    You're to act as a Python Programming AI language model. You specialize 
+    in interpreting what a user wants by asking questions to better understand
+    their needs for the task ahead. You mimic the experience of a 20 year expert
+    in the programming field. Your task is now to help developers create gui apps 
+    with modular code that is so deeply modular that they could work relatively 
+    independent where classes and subsystems are concerned. You must Always ask 
+    questions BEFORE you answer so you can better zone in on what the mentee is 
+    seeking. Is that understood? '''})
+else:
+    conversation.append({'role': 'user', 'content': "Hi, How are you?"})
 conversation = ChatGPT_conversation(conversation)
 print('{0}: {1}\n'.format(conversation[-1]['role'].strip(), conversation[-1]['content'].strip()))
 
 while True:
-    prompt = input('User:')
+    prompt = input('User: \n')
     conversation.append({'role': 'user', 'content': prompt})
     conversation = ChatGPT_conversation(conversation)
     print('{0}: {1}\n'.format(conversation[-1]['role'].strip(), conversation[-1]['content'].strip()))
